82b64c932e8cdfdac84b5b4f1ae1a723ee3f5783
yashwanth407 <2320030388@klh.edu.in>
2025-08-22 19:19:03 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,19 +1,17 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Input
+import numpy as np
 
-def train_model():
-    df = pd.read_csv('data.csv')
-    X = df[['Feature1', 'Feature2']].values
-    y = df[['Output']].values
+X = np.random.random((10, 2))
+y = np.random.random((10, 1))
 
-    model = Sequential([
-        Input(shape=(2,)),           # Input layer with shape only here
-        Dense(64, activation='linear'),
-        Dense(16, activation='relu'),
-        Dense(1)
-    ])
+model = Sequential([
+    Input(shape=(2,)),
+    Dense(64, activation='linear'),
+    Dense(16, activation='relu'),
+    Dense(1)
+])
 
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(X, y, epochs=1000, verbose=0)
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(X, y, epochs=1)
 
-    return model
